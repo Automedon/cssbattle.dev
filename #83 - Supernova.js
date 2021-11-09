416ceff02fd7489c9c4d37cf6357d1e70ddb900d
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2021-11-09 08:31:25 +0700

    Create #83 - Supernova.js

---
+++ #83 - Supernova.js
@@ -0,0 +1,47 @@
+<div a></div>
+<div b></div>
+<div c></div>
+<div d></div>
+<style>
+  *{
+    background:#243D83;
+  }
+  [a] {
+    position:absolute;
+    left:90;
+    top:100;
+    background:#6592CF;
+    width: 220px;
+    height: 100px;
+    transform:rotate(45deg);
+    border-radius:70px;
+  }
+  [b]{
+    position:absolute;
+    left:90;
+    top:100;
+    background:#6592CF;
+    width: 220px;
+    height: 100px;
+    transform:rotate(-45deg);
+    border-radius:70px;
+  }
+  [c]{
+    position:absolute;
+    left:140;
+    top:90;
+    background:#243D83;
+    width: 120px;
+    height: 120px;
+    transform:rotate(-45deg);
+  }
+  [d]{
+    position:absolute;
+    left:170;
+    top:120;
+    background:#EEB850;
+    width: 60px;
+    height: 60px;
+    border-radius:70px;
+  }
+</style>
