0b52df0ee766385052ad023d027103c7153c07c7
Valefar TypeScript Machine <42299107+Automedon@users.noreply.github.com>
2022-04-05 10:02:22 +0700

    Create #108 - Clow Card.js

---
+++ #108 - Clow Card.js
@@ -0,0 +1,88 @@
+<div a></div>
+<div b></div>
+<div c></div>
+<div d></div>
+<div e></div>
+<div f></div>
+<div g></div>
+<div j></div>
+<style>
+  *{
+    margin:0;
+    background:#000000;
+  }
+  [a] {
+    position:absolute;
+    top:75;
+    left:50;
+    width: 300px;
+    height: 150px;
+    background: #E96A23;
+  }
+  [b] {
+    position:absolute;
+    top:75;
+    left:155;
+    width: 50px;
+    height: 150px;
+    background: #EBAE11;
+    transform:skewX(-15deg)
+  }
+  [c] {
+    position:absolute;
+    top:75;
+    left:220;
+    width: 25px;
+    height: 150px;
+    background: #EBAE11;
+    transform:skewX(-15deg)
+  }
+  [d] {
+    position:absolute;
+    top:105;
+    left:65;
+    width: 270px;
+    height: 90px;
+    background: #000000;
+	border-radius:13px;
+  }
+  [e] {
+    position:absolute;
+    top:90;
+    left:80;
+    width: 240px;
+    height: 120px;
+    background: #000000;
+	border-radius:13px;
+  }
+  [f] {
+    position:absolute;
+    top:105;
+    left:95;
+    width: 200px;
+    height: 80px;
+    background: transparent;
+    border:5px solid #EBAE11;
+	border-radius:50%;
+  }
+  [g] {
+    position:absolute;
+    top:115;
+    left:135;
+    width: 120px;
+    height: 60px;
+    background: transparent;
+    border:5px solid #EBAE11;
+	border-radius:50%;
+  }
+  [j] {
+    position:absolute;
+    top:125;
+    left:175;
+    width: 50px;
+    height: 50px;
+    background: #EBAE11;
+
+	border-radius:50%;
+  }
+</style>
